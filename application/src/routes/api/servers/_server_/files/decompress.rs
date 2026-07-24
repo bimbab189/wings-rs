aebee778450708a0fb1c5f904b9ef34c99e086b2
Robert Jansen <me@rjns.dev>
2026-07-24 11:28:28 +0200

    fix fs resolving in decompression endpoint

--- application/src/routes/api/servers/_server_/files/decompress.rs
+++ application/src/routes/api/servers/_server_/files/decompress.rs
@@ -65,6 +65,17 @@ mod post {
                 .ok();
         }
 
+        let (destination_root, destination_filesystem) =
+            server.filesystem.resolve_writable_fs(&server, &root).await;
+
+        if destination_filesystem.is_primary_server_fs()
+            && server.filesystem.is_ignored(&root, true)
+        {
+            return ApiResponse::error("root not found")
+                .with_status(StatusCode::NOT_FOUND)
+                .ok();
+        }
+
         let source = root.join(data.file);
 
         if server.filesystem.is_ignored(
@@ -97,9 +108,6 @@ mod post {
         let progress = Arc::new(AtomicU64::new(0));
         let total = Arc::new(AtomicU64::new(0));
         let files_processed = Arc::new(AtomicU64::new(0));
-
-        let (destination_root, destination_filesystem) =
-            server.filesystem.resolve_writable_fs(&server, &root).await;
 
         let (identifier, task) = server
             .filesystem
